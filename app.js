c73c75dc70c32aece04f80655d81ebb11ea9a383
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-05-03 13:20:05 -0700

    Users can edit and update albums

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 // require handlebars
 var exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -14,13 +15,15 @@ const bodyParser = require('body-parser');
 const models = require('./db/models');
 
 
+
 // OUR MOCK ARRAY OF PROJECTS
 var albums = [
   { title: "I am your first album", desc: "A great album that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "I am your second album", desc: "A great album that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "I am your third album", desc: "A great album that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
-
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'));
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -52,6 +55,26 @@ app.get('/albums/:id', (req, res) => {
     // if they id was for an album not in our db, log an error
     console.log(err.message);
   })
+});
+// EDIT
+app.get('/albums/:id/edit', (req, res) => {
+  models.Album.findByPk(req.params.id).then((album) => {
+    res.render('albums-edit', { album: album });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+// UPDATE
+app.put('/albums/:id', (req, res) => {
+  models.Album.findByPk(req.params.id).then(album => {
+    album.update(req.body).then(album => {
+      res.redirect(`/albums/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
